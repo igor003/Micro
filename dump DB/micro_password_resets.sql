32c9252281d9b192877af1924972a9fed32dc508
igor003 <igorio6ka1995@gmail.com>
2021-01-04 16:47:58 +0200

    Removed folder DB2 from repository

--- dump DB/micro_password_resets.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE micro.password_resets
-(
-    email varchar(255) NOT NULL,
-    token varchar(255) NOT NULL,
-    created_at timestamp
-);
-CREATE INDEX password_resets_email_index ON micro.password_resets (email);
